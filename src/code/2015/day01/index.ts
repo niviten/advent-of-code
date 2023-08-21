808cac577451fe31b279ef322d9dd88fb5bf3feb
niviten <l.j.nivethan@gmail.com>
2023-08-21 08:08:09 +0530

    Add 2015 Day 01 solution

---
+++ src/code/2015/day01/index.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs'
+
+function first(input: string): number {
+  let floor: number = 0
+  for (let i: number = 0; i < input.length; i++) {
+    const ch: string | undefined = input[i]
+    if (ch === undefined) {
+      continue
+    }
+    if (ch === '(') {
+      floor = floor + 1
+    } else if (ch === ')') {
+      floor = floor - 1
+    }
+  }
+  return floor
+}
+
+function second(input: string): number {
+  let floor: number = 0
+  for (let i: number = 0; i < input.length; i++) {
+    const ch: string | undefined = input[i]
+    if (ch === undefined) {
+      continue
+    }
+    if (ch === '(') {
+      floor = floor + 1
+    } else if (ch === ')') {
+      floor = floor - 1
+    }
+    if (floor === -1) {
+      return i + 1
+    }
+  }
+  return -1
+}
+
+function firstWrapper() {
+  const filePath: string = './input.txt'
+  const input: string = fs.readFileSync(filePath, 'utf-8')
+  const output: number = first(input)
+  console.log('first output', output)
+}
+
+function secondWrapper() {
+  const filePath: string = './input.txt'
+  const input: string = fs.readFileSync(filePath, 'utf-8')
+  const output: number = second(input)
+  console.log('second output', output)
+}
+
+firstWrapper()
+secondWrapper()
